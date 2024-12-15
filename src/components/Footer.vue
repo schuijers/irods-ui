5b6003ed9998af7b0a933f287157ce33c6d29db7
Martijn Schuijers <martijnschuijers@gmail.com>
2024-12-15 13:45:32 +0100

    feature: add GitHub link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,5 +8,18 @@
     >
       Built with ❤️ by <span class="text-gray-900 dark:text-white">Martijn Schuijers</span>
     </a>
+
+    <ISpacer />
+
+    <UButton
+      aria-label="iRODS UI on GitHub"
+      color="gray"
+      icon="i-simple-icons-github"
+      size="sm"
+      square
+      target="_blank"
+      to="https://github.com/schuijers/irods-ui"
+      variant="ghost"
+    />
   </footer>
 </template>
